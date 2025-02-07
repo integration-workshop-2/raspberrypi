1742036f035d77289e6ce4cd9f8e42bd84cf805f
jhcsoares1 <jhcsoares1@gmail.com>
2025-02-06 23:42:17 -0300

    fixed url parameter

--- server/routes/control_motor.py
+++ server/routes/control_motor.py
@@ -6,8 +6,9 @@ from data.use_cases.routine.control_motor.use_case import ControlMotorUseCase
 control_motor_bp = Blueprint("control_motor_bp", __name__)
 
 
-@control_motor_bp.route("/control_motor/<str:cylinder_number>", methods=["GET"])
-def get_readings_from_vital_signs_sensors(cylinder_number: str):
-    parameter = ControlMotorParameter(cylinder_number=int(cylinder_number))
-    response = use_case.execute()
+@control_motor_bp.route("/control_motor/<int:cylinder_number>", methods=["GET"])
+def get_readings_from_vital_signs_sensors(cylinder_number: int):
+    parameter = ControlMotorParameter(cylinder_number=cylinder_number)
+    use_case = ControlMotorUseCase()
+    response = use_case.execute(parameter=parameter)
     return jsonify(response), 200
