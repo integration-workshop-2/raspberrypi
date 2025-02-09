9df8bd649d18fad77266bd5585e69c6edcb05d6d
jhcsoares1 <jhcsoares1@gmail.com>
2025-02-09 20:35:58 -0300

    formatting response

--- infra/hardware/max30102/heartrate_monitor.py
+++ infra/hardware/max30102/heartrate_monitor.py
@@ -1,18 +1,17 @@
+import numpy as np
+
 from .max30102 import MAX30102
 from .hrcalc import calc_hr_and_spo2
-import time
-import numpy as np
+from infra.hardware.max30102.models.max30102_response import MAX30102Response
 
 
 class HeartRateMonitor(object):
-    def __init__(self, print_raw=False, print_result=True):
+    def __init__(self, print_raw=False, print_result=True) -> None:
         self.bpm = 0
-        if print_raw is True:
-            print("IR, Red")
         self.print_raw = print_raw
         self.print_result = print_result
 
-    def get_readings(self):
+    def get_readings(self) -> MAX30102Response:
         sensor = MAX30102()
         ir_data = []
         red_data = []
@@ -31,8 +30,6 @@ class HeartRateMonitor(object):
                     num_bytes -= 1
                     ir_data.append(ir)
                     red_data.append(red)
-                    if self.print_raw:
-                        print("{0}, {1}".format(ir, red))
 
                 while len(ir_data) > 100:
                     ir_data.pop(0)
@@ -52,5 +49,8 @@ class HeartRateMonitor(object):
                             if self.print_result:
                                 print("Finger not detected")
                         if self.print_result:
-                            has_read = True
-                            print("BPM: {0}, SpO2: {1}".format(self.bpm, spo2))
+                            if spo2 != -999:
+                                has_read = True
+                                return MAX30102Response(
+                                    bpm=self.bpm, oxygenation_percentage=spo2
+                                )
