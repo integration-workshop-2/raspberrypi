ba17a72964165830d71571ef49e5860275753a84
jhcsoares1 <jhcsoares1@gmail.com>
2025-02-21 20:06:34 -0300

    added anti-clockwise movement

--- data/use_cases/routine/control_motor/use_case.py
+++ data/use_cases/routine/control_motor/use_case.py
@@ -13,14 +13,38 @@ class ControlMotorUseCase:
         self.__motor = Motor(cylinder_number=parameter.cylinder_number)
 
         stop = False
+        rounds_counter = 0
 
         while not stop:
-            for i in range(8):
-                for j in range(4):
-                    self.__motor.set_pin_state(
-                        pin=self.__motor.get_control_pins()[j],
-                        state=self.__motor.get_half_step_sequence()[i][j],
-                    )
-                    if self.__tcrt5000a.detected or self.__tcrt5000b.detected:
-                        stop = True
-                sleep(0.001)
+            if rounds_counter < 2 * 512:
+                for i in range(8):
+                    for j in range(4):
+                        self.__motor.set_pin_state(
+                            pin=self.__motor.get_control_pins()[j],
+                            state=self.__motor.get_half_step_sequence()[i][j],
+                        )
+
+                        if self.__tcrt5000a.detected or self.__tcrt5000b.detected:
+                            stop = True
+
+                    sleep(0.001)
+
+                rounds_counter += 1
+
+            elif rounds_counter < 3 * 512:
+                for k in range(8):
+                    for l in range(4):
+                        self.__motor.set_pin_state(
+                            pin=self.__motor.get_control_pins()[::-1][l],
+                            state=self.__motor.get_half_step_sequence()[k][l],
+                        )
+
+                        if self.__tcrt5000a.detected or self.__tcrt5000b.detected:
+                            stop = True
+
+                    sleep(0.001)
+
+                rounds_counter += 1
+
+            else:
+                rounds_counter = 0
